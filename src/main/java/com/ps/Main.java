1f793dad68a7b1159e0abdcea9c5b096fccb1801
Wai_Yan <waiyanbhonemyint2002@gmail.com>
2025-04-16 11:55:15 -0400

    project done

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -12,15 +12,27 @@ public class Main {
         //split user's input with pipe sign
         String[] team = userInput.split("\\|");
 
-        //make array for team names
-        String teamNames = team[0];
-        String scoreArray = team[1];
+        //make array for team names and each will with separate by ":"
+        String teams = team[0];
+        String scores = team[1];
 
+        //split teams and scores with ":" and generate arrays
+        String[] teamNames = teams.split(":");
+        String[] teamScores = scores.split(":");
 
+        //convert string type number to integer
+        int firstIndex = Integer.parseInt(teamScores[0]);
+        int secondIndex = Integer.parseInt(teamScores[1]);
 
-
-
+        //check the conditional statement between two team scores
+        if (firstIndex > secondIndex){
+            System.out.print("The Winner is " + teamNames[0]);
             }
+        else if (secondIndex > firstIndex) {
+            System.out.print("The Winner is " + teamNames[1]);
+        }
+        else{
+            System.out.print("Draw");
         }
     }
 }
